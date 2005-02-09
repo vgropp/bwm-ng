04f545d2763ce8e0a851e6fbeff2efc2ce6c79bb
vgropp <vgropp@9d038f78-4a1f-0410-99f9-a7e0acc80b06>
2005-02-09 12:08:46 +0000

    clean up and reorder
    git-svn-id: https://svn.code.sf.net/p/bwmng/code/trunk@38 9d038f78-4a1f-0410-99f9-a7e0acc80b06

--- src/defines.h
+++ src/defines.h
@@ -21,6 +21,14 @@
  *                                                                            *
  *****************************************************************************/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+#include <string.h>
+
 #include "../config.h"
 
 /* ugly defines to handle different compile time options */
@@ -37,20 +45,16 @@
 #define LIBSTATGRAB
 #endif
 
-#if HAVE__PROC_NET_DEV
-#ifdef PROC_NET_DEV_PATH
-#define PROC_NET_DEV PROC_NET_DEV_PATH
-#else
-#define PROC_NET_DEV "/proc/net/dev"
-#endif
-#endif
-
 #if HAVE_LIBCURSES || HAVE_LIBNCURSES
 #define HAVE_CURSES
 #endif
 
 #if HAVE_STRUCT_IF_DATA_IFI_IBYTES
 #define GETIFADDRS
+#endif
+
+#if HAVE_GETOPT_LONG
+#define LONG_OPTIONS
 #endif
 
 #if HAVE_IOCTL
@@ -60,8 +64,17 @@
 #endif
 #endif
 
-#if HAVE_GETOPT_LONG
-#define LONG_OPTIONS
+#ifdef HAVE_CURSES
+#include <curses.h>
+#endif
+
+#ifdef LONG_OPTIONS
+#include <getopt.h>
+#endif
+
+#if HAVE_GETTIMEOFDAY
+#include <sys/time.h>
+#include <time.h>
 #endif
 
 #ifdef NETSTAT
@@ -72,164 +85,122 @@
 #endif
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-
-#ifdef HAVE_CURSES
-#include <curses.h>
+#if HAVE__PROC_NET_DEV
+#ifdef PROC_NET_DEV_PATH
+#define PROC_NET_DEV PROC_NET_DEV_PATH
+#else
+#define PROC_NET_DEV "/proc/net/dev"
+#endif
 #endif
 
-#ifdef LONG_OPTIONS
-#include <getopt.h>
-#endif
-
-#include <limits.h>
-#include <signal.h>
-#include <string.h>
-
-#if HAVE_GETTIMEOFDAY
-#include <sys/time.h>
-#include <time.h>
-#endif
-
-
-#define MAJOR 0
-#define MINOR 5
-#define EXTRA "-pre2-cvs"
-
-#define WRAP_AROUND ULONG_MAX
-#define WRAP_32BIT 4294967295ul
-
-#ifdef SYS_64BIT 
-#undef WRAP_AROUND
-#define WRAP_AROUND 18446744073709551615ull
-#endif
-
-#ifdef HAVE_CURSES
-#define CURSES_OUT 0
-#endif
-#define PLAIN_OUT 1
-#ifdef CSV
-#define CSV_OUT 2
-#endif
-#ifdef HTML
-#define HTML_OUT 3
-#endif
-#define PLAIN_OUT_ONCE 4
-
-
+/* prepare input methods */
 #ifdef NETSTAT
 #define NETSTAT_IN 1
-#define INPUT_0 1
-#define INPUT_METHODS_0 " netstat"
-#else 
-#define INPUT_METHODS_0 ""
-#define INPUT_0 0
+#define INPUT_METHODS_NETSTAT " netstat"
+#else
+#define NETSTAT_IN 0
+#define INPUT_METHODS_NETSTAT
 #endif
 
 #ifdef PROC_NET_DEV
 #define PROC_IN 2
-#define INPUT_1 (INPUT_0 | 2)
-#define INPUT_METHODS_1 INPUT_METHODS_0 " proc"
+#define INPUT_METHODS_PROC " proc"
 #else
-#define INPUT_1 INPUT_0
-#define INPUT_METHODS_1 INPUT_METHODS_0
+#define PROC_IN 0
+#define INPUT_METHODS_PROC
 #endif
 
 #ifdef GETIFADDRS
 #define GETIFADDRS_IN 4
-#define INPUT_2 (INPUT_1 | 4)
-#define INPUT_METHODS_2 INPUT_METHODS_1 " getifaddrs"
+#define INPUT_METHODS_GETIFADDRS " getifaddrs"
 #else
-#define INPUT_METHODS_2 INPUT_METHODS_1
-#define INPUT_2 INPUT_1
+#define GETIFADDRS_IN 0
+#define INPUT_METHODS_GETIFADDRS
 #endif
 
 #ifdef LIBSTATGRAB
 #define LIBSTAT_IN 8
-#define INPUT_3 (INPUT_2 | 8)
-#define INPUT_METHODS_3 INPUT_METHODS_2 " libstatgrab"
+#define INPUT_METHODS_LIBSTATGRAB " libstatgrab"
 #else
-#define INPUT_METHODS_3 INPUT_METHODS_2
-#define INPUT_3 INPUT_2
+#define LIBSTAT_IN 0
+#define INPUT_METHODS_LIBSTATGRAB
 #endif
 
 #ifdef SYSCTL
 #define SYSCTL_IN 16
-#define INPUT_4 (INPUT_3 | 16)
-#define INPUT_METHODS_4 INPUT_METHODS_3 " sysctl"
+#define INPUT_METHODS_SYSCTL " sysctl"
 #else
-#define INPUT_METHODS_4 INPUT_METHODS_3
-#define INPUT_4 INPUT_3
+#define SYSCTL_IN 0
+#define INPUT_METHODS_SYSCTL
 #endif
 
 #if HAVE_LIBKSTAT
 #define KSTAT_IN 32
-#define INPUT_MASK (INPUT_4 | 32)
-#define INPUT_METHODS INPUT_METHODS_4 " kstat"
-#else
-#define INPUT_MASK INPUT_4
-#define INPUT_METHODS INPUT_METHODS_4
+#define INPUT_METHODS_KSTAT " kstat"
+#else 
+#define KSTAT_IN 0
+#define INPUT_METHODS_KSTAT
 #endif
 
-
-
+#define INPUT_MASK (NETSTAT_IN | PROC_IN | GETIFADDRS_IN | LIBSTAT_IN | SYSCTL_IN | KSTAT_IN)
+#define INPUT_METHODS INPUT_METHODS_PROC INPUT_METHODS_GETIFADDRS INPUT_METHODS_SYSCTL INPUT_METHODS_KSTAT INPUT_METHODS_NETSTAT INPUT_METHODS_LIBSTATGRAB 
 
 /* used for this nice spinning wheel */
 #define IDLE_CHARS "-\\|/"
 
 
 /* build output methods string: curses, plain, csv, html */
-#define OUTPUT_METHODS_0 "plain"
-
 #ifdef HAVE_CURSES 
-#define OUTPUT_METHODS_1 OUTPUT_METHODS_0 ", curses"
-#else 
-#define OUTPUT_METHODS_1 OUTPUT_METHODS_0
+#define CURSES_OUTPUT_METHODS ", curses"
+#define CURSES_OUT 0
+#else
+#define CURSES_OUTPUT_METHODS
 #endif
 
 #ifdef CSV
-#define OUTPUT_METHODS_2 OUTPUT_METHODS_1 ", csv"
+#define CSV_OUTPUT_METHODS ", csv"
+#define CSV_OUT 2
 #else
-#define OUTPUT_METHODS_2 OUTPUT_METHODS_1
+#define CSV_OUTPUT_METHODS
 #endif
 
 #ifdef HTML
-#define OUTPUT_METHODS OUTPUT_METHODS_2 ", html"
-#else 
-#define OUTPUT_METHODS OUTPUT_METHODS_2
+#define HTML_OUTPUT_METHODS ", html"
+#define HTML_OUT 3
+#else
+#define HTML_OUTPUT_METHODS
 #endif
+
+#define OUTPUT_METHODS "plain" CURSES_OUTPUT_METHODS CSV_OUTPUT_METHODS HTML_OUTPUT_METHODS
+#define PLAIN_OUT 1
+#define PLAIN_OUT_ONCE 4
 
 /* build short options */
 #ifdef PROC_NET_DEV
-#define SHORT_OPTIONS_0 ":ht:f:dVa:u:I:i:o:c:DST:"
-#else
-#define SHORT_OPTIONS_0 ":ht:dVa:u:I:i:o:c:DST:"
+#define PROC_SHORT_OPT "f:"
+#else 
+#define PROC_SHORT_OPT
 #endif
 
 #ifdef HTML
-#define SHORT_OPTIONS_1 SHORT_OPTIONS_0 "HR:"
+#define HTML_SHORT_OPT "HR:"
 #else
-#define SHORT_OPTIONS_1 SHORT_OPTIONS_0
+#define HTML_SHORT_OPT
 #endif
 
-#if (NETSTAT_LINUX || NETSTAT_BSD) && NETSTAT_OPTION
-#define SHORT_OPTIONS_2 SHORT_OPTIONS_1 "n:"
-#else
-#ifdef NETSTAT
-#define SHORT_OPTIONS_2 SHORT_OPTIONS_1 "n"
+#if NETSTAT && NETSTAT_OPTION
+#define NETSTAT_SHORT_OPT "n:"
 #else 
-#define SHORT_OPTIONS_2 SHORT_OPTIONS_1
-#endif
+#define NETSTAT_SHORT_OPT
 #endif
 
 #ifdef CSV
-#define SHORT_OPTIONS SHORT_OPTIONS_2 "C:F:"
-#else
-#define SHORT_OPTIONS SHORT_OPTIONS_2
+#define CSV_SHORT_OPT "C:F:"
+#else 
+#define CSV_SHORT_OPT
 #endif
+
+#define SHORT_OPTIONS ":ht:dVa:u:I:i:o:c:DST:" PROC_SHORT_OPT HTML_SHORT_OPT NETSTAT_SHORT_OPT CSV_SHORT_OPT
 
 #define BYTES_OUT 1
 #define BITS_OUT 2
@@ -241,11 +212,23 @@
 #define SUM_OUT 3
 #define AVG_OUT 4
 
-/* length of avg in 1/1000sec */
+/* default length of avg in 1/1000sec */
 #define AVG_LENGTH 30000
 
 #ifndef PATH_MAX
 #define PATH_MAX 4096
 #endif
+
+#define WRAP_AROUND ULONG_MAX
+#define WRAP_32BIT 4294967295ul
+
+#ifdef SYS_64BIT
+#undef WRAP_AROUND
+#define WRAP_AROUND 18446744073709551615ull
+#endif
+
+#define MAJOR 0
+#define MINOR 5
+#define EXTRA "-pre2-cvs"
 
 #define print_version printf("Bandwidth Monitor NG (bmw-ng) v%i.%i%s\nCopyright (C) 2004,2005 Volker Gropp <bwmng@gropp.org>\n",MAJOR,MINOR,EXTRA); 

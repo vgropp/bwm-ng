002ebcb67578d4d6312d0aabfc81a4d3bc7bf0e3
vgropp <vgropp@9d038f78-4a1f-0410-99f9-a7e0acc80b06>
2007-02-19 12:26:46 +0000

    fix kstat configure and disk input
    git-svn-id: https://svn.code.sf.net/p/bwmng/code/trunk@98 9d038f78-4a1f-0410-99f9-a7e0acc80b06

--- src/retrieve.c
+++ src/retrieve.c
@@ -424,7 +424,8 @@ void get_iface_stats_sysctl (char verbose) {
 void get_iface_stats_kstat (char verbose) {
     kstat_ctl_t   *kc;
     kstat_t       *ksp;
-    kstat_named_t *i_bytes,*o_bytes,*i_packets,*o_packets,*i_errors,*o_errors;
+    kstat_io_t     kio;	
+    kstat_named_t *i_bytes=NULL,*o_bytes=NULL,*i_packets=NULL,*o_packets=NULL,*i_errors=NULL,*o_errors=NULL;
     char *name;
     int hidden_if=0,current_if_num=0,my_errno=0;
     t_iface_speed_stats tmp_if_stats;
@@ -440,12 +441,12 @@ void get_iface_stats_kstat (char verbose) {
 	 
     /* loop for interfaces */
     for (ksp = kc->kc_chain;ksp != NULL;ksp = ksp->ks_next) {
-        if ((strcmp(ksp->ks_class, "net") != 0 && input_method==KSTAT_IN) || (strcmp(ksp->ks_class, "disk") != 0 && input_method==KSTATDISK_IN))
+        if ((strcmp(ksp->ks_class, "net") != 0 && input_method==KSTAT_IN) || (strcmp(ksp->ks_class, "disk") != 0 && input_method==KSTATDISK_IN && ksp->ks_type != KSTAT_TYPE_IO))
             continue; /* skip all other stats */
         strncpy(name,ksp->ks_name,KSTAT_STRLEN);
         name[KSTAT_STRLEN-1]='\0';
-        kstat_read(kc, ksp, NULL);
 		  if (KSTAT_IN==input_method) {
+		kstat_read(kc, ksp, NULL);
 	        i_bytes=(kstat_named_t *)kstat_data_lookup(ksp, "rbytes");
 		     o_bytes=(kstat_named_t *)kstat_data_lookup(ksp, "obytes");
 			  i_packets=(kstat_named_t *)kstat_data_lookup(ksp, "ipackets");
@@ -454,30 +455,24 @@ void get_iface_stats_kstat (char verbose) {
 			  o_errors=(kstat_named_t *)kstat_data_lookup(ksp, "oerrors");
 	        if (!i_bytes || !o_bytes || !i_packets || !o_packets || !i_errors || !o_errors) 
 		         continue;
-		  } else if (KSTATDISK_IN==input_method) {
-			  i_bytes=(kstat_named_t *)kstat_data_lookup(ksp, "nread");
-			  o_bytes=(kstat_named_t *)kstat_data_lookup(ksp, "nwritten");
-			  i_packets=(kstat_named_t *)kstat_data_lookup(ksp, "reads");
-			  o_packets=(kstat_named_t *)kstat_data_lookup(ksp, "writes");
-			  if (!i_bytes || !o_bytes || !i_packets || !o_packets)
-				  continue;
-		  } else {
-			  free(name);
-			  deinit(1,"im confused about kstat input methods!\n");
-		  }
-        /* use ui32 values, the 64 bit values return strange (very big) differences */
+	/* use ui32 values, the 64 bit values return strange (very big) differences */
         tmp_if_stats.bytes.in=i_bytes->value.ui32;
         tmp_if_stats.bytes.out=o_bytes->value.ui32;
         tmp_if_stats.packets.in=i_packets->value.ui32;
         tmp_if_stats.packets.out=o_packets->value.ui32;
-        if (i_errors) /* disk input wont set this */ 
-			  tmp_if_stats.errors.in=i_errors->value.ui32 
-		  else
-			  tmp_if_stats.errors.in=0;
-		  if (o_errors) /* disk input wont set this */
-	        tmp_if_stats.errors.out=o_errors->value.ui32;
-		  else
-			  tmp_if_stats.errors.out=0;
+	tmp_if_stats.errors.in=i_errors->value.ui32;
+	tmp_if_stats.errors.out=o_errors->value.ui32;
+		  } else if (KSTATDISK_IN==input_method) {
+			kstat_read(kc, ksp, &kio);  	
+			tmp_if_stats.bytes.in=kio.nread;
+			tmp_if_stats.bytes.out=kio.nwritten;
+			tmp_if_stats.packets.in=kio.reads;
+			tmp_if_stats.packets.out=kio.writes;
+			tmp_if_stats.errors.in=tmp_if_stats.errors.out=0;
+		  } else {
+			  free(name);
+			  deinit(1,"im confused about kstat input methods!\n");
+		  }
         /* init new interfaces and add fetched data to old or new one */
         hidden_if = process_if_data (hidden_if, tmp_if_stats, &stats, name, current_if_num, verbose, 1);
         current_if_num++;
